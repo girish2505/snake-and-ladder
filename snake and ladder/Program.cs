51c981622859a693882692098a1c90c51bcfe9d2
Vayugundla Girish Guptha <girishvayugundla@gmail.com>
2021-06-30 18:15:51 +0530

    snake and ladder uc1

---
+++ snake and ladder/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace snake_and_ladder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("snake and ladder game !!!!");
+            RollDice rd = new RollDice();
+            rd.StartGame();
+        }
+    }
+}
---
+++ snake and ladder/RollDice.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace snake_and_ladder
+{
+    class RollDice
+    {
+        public static int Start_Position = 0;
+        public void StartGame()
+        {
+            Console.WriteLine("Player position start with : "+Start_Position);
+        }
+    }
+}
